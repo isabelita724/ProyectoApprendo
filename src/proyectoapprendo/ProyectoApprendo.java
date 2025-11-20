96bf5ea2df1a09b5e97275b11973052c116209fc
Rafo0910 <rafaj0910@gmail.com>
2025-11-19 21:59:13 -0500

    feat: merge de admin

--- src/proyectoapprendo/ProyectoApprendo.java
+++ src/proyectoapprendo/ProyectoApprendo.java
@@ -1,24 +1,21 @@
 package proyectoapprendo;
 
-<<<<<<< HEAD
-=======
 import model.GestorUsuarios;
 
 /**
  *
  * @author ISABELLA
  */
->>>>>>> feature/login
 public class ProyectoApprendo {
 
     public static void main(String[] args) {
+
         GestorUsuarios.inicializarUsuariosBase();
-        
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true); 
+                new Login().setVisible(true);
             }
         });
     }
-    
 }
